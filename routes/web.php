c45c6e47f8faea9a74d2e7f2317fd2cda0ff9a77
EvanKs <evanks@rocketmail.com>
2020-08-30 19:55:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,49 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "saya ganteng";
+});
+
+
+// Route::get("/{asdf}", function($asdf){
+//     return "$asdf";
+// });
+
+Route::get("/hello", function(){
+    return "Hello World!";
+});
+
+Route::get('/wkwkwk', function(){
+    $arr = array("nama" => "evan");
+    dd($arr);
+    //die dump
+});
+
+Route::get('/tesredirect', function () {
+    return redirect("/hello");
+ })->name("r");
+
+Route::get('/tokped/{toko?}/{barang?}', function ($toko=null, $barang=null) {
+    if(!$toko){
+        //return redirect("https://google.com");
+        return redirect()->route("r");
+    }
+     if($toko == "stts"){
+         return "WOE STTS BUKAN TOKO!";
+     }
+     else{
+        return "TOKO <h1>$toko</h1><br> dijual cepat $barang";
+     }
+});
+
+Route::prefix('/jadwal')->group(function () {
+    Route::get('/ajar', function () {
+        return "jadwal ajar";
+    });
+    Route::get('/ruang', function () {
+        return "jadwal ruang";
+    });
+    Route::get('/dosen', function () {
+        return "jadwal dosen";
+    });
 });
